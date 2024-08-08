18bb2477463a64ac4a759c8c8fc7a56ac18162d5
Tiago Santos <tiagoalmeidasantos04@gmail.com>
2024-08-08 19:38:07 -0300

    complete

--- src/app.tsx
+++ src/app.tsx
@@ -1,24 +1,27 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import CreateRoom from "./pages/create-room";
-import Room from "./pages/room";
-import { Toaster } from "sonner";
+import { Toaster } from 'sonner'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { CreateRoom } from './pages/create-room'
+import { Room } from './pages/room'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <CreateRoom />,
+    path: '/',
+    element: <CreateRoom />
   },
   {
-    path: "/room/:roomId",
-    element: <Room />,
-  },
-]);
+    path: '/room/:roomId',
+    element: <Room />
+  }
+])
 
 export function App() {
   return (
-    <>
-      <RouterProvider router={router} />;
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
       <Toaster invert richColors />
-    </>
-  );
+    </QueryClientProvider>
+  )
 }
+
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { App } from "./app"; // Fixed casing of file name
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { App } from './app'
+import './index.css'
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
-);
+  </React.StrictMode>,
+)
